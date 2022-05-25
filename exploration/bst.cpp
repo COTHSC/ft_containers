c5d69b75aed14708632665ee03ea11516f312690
COTHSC <thierryscully.colomban@gmail.com>
2022-05-25 11:23:25 +0200

    fixed some memory leaks

--- exploration/bst.cpp
+++ exploration/bst.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+
+enum color_t { BLACK, RED };
 
 class leaf {
 public:
+  // TODO this constructor is used to insert new leaves with a value, I really
+  // shouldnt need  the default sentinel value here...
   leaf(int key, std::string value, leaf *root, leaf *sentinel,
        int is_sentinel = 0)
-      : key(key), value(value), _sentinel(is_sentinel), right(sentinel),
-        root(root), left(sentinel){};
+      : key(key), value(value), _sentinel(is_sentinel), parent(root),
+        color(RED) {
+    children[1] = sentinel;
+    children[0] = sentinel;
+  };
+
   leaf(int sentinel = 0) : _sentinel(sentinel), key(0), value(" "){};
-  leaf(leaf *sentinel) : right(sentinel), left(sentinel){};
+  leaf(leaf *sentinel) {
+    children[1] = sentinel;
+    children[0] = sentinel;
+  };
   ~leaf(){};
-  leaf *right;
-  leaf *left;
-  leaf *root;
+  // leaf *right;
+  // leaf *left;
+  leaf *parent;
+  leaf *children[2];
+  // index is left = 0
+  // right = 1
   std::string value;
   int key;
   bool _sentinel;
+  enum color_t color;
 
 private:
 };
@@ -23,76 +40,68 @@ class tree {
 public:
   tree() {
     sentinel = new leaf(1);
+    root = sentinel;
+    cursor = root;
     size = 0;
   };
-  ~tree(){};
+  ~tree() {
+    deallocate(root);
+    delete sentinel;
+  };
 
+  void deallocate(leaf *node) {
+    if (node->_sentinel)
+      return;
+    deallocate(node->children[0]);
+    deallocate(node->children[1]);
+    delete node;
+  }
   leaf *min() { return min_rec(root); }
   leaf *max() { return max_rec(root); }
   leaf *min(leaf *subtree) { return min_rec(subtree); }
   leaf *max(leaf *subtree) { return max_rec(subtree); }
-
-  // BST-Successor(x)
-  //   if x.right ≠ NIL then
-  //     return Tree-Minimum(x.right)
-  //   end if
-  //   y := x.parent
-  //   while y ≠ NIL and x = y.right then
-  //     x := y
-  //     y := y.parent
-  //   repeat
-  //   return y
 
   leaf *next(int key) {
     cursor = root;
     cursor = find(key);
     if (cursor->_sentinel)
       return sentinel;
-    if (!cursor->right->_sentinel)
-      return min(cursor->right);
-    leaf *parent = cursor->root;
-    while (!parent->_sentinel && cursor == parent->right) {
+    if (!cursor->children[1]->_sentinel)
+      return min(cursor->children[1]);
+    leaf *parent = cursor->parent;
+    while (!parent->_sentinel && cursor == parent->children[1]) {
       cursor = parent;
-      parent = parent->root;
+      parent = parent->parent;
     }
     return parent;
   }
-  // BST-Predecessor(x)
-  //    if x.left ≠ NIL then
-  //      return Tree-Maximum(x.left)
-  //    end if
-  //    y := x.parent
-  //    while y ≠ NIL and x = y.left then
-  //      x := y
-  //      y := y.parent
-  //    repeat
-  //    return y
+
   leaf *prev(int key) {
     cursor = find(key);
     if (cursor->_sentinel)
       return sentinel;
-    if (!cursor->left->_sentinel)
-      return max(cursor->left);
-    leaf *parent = cursor->root;
-    while (!parent->_sentinel && cursor == parent->left) {
+    if (!cursor->children[0]->_sentinel)
+      return max(cursor->children[0]);
+    leaf *parent = cursor->parent;
+    while (!parent->_sentinel && cursor == parent->children[0]) {
       cursor = parent;
-      parent = parent->root;
+      parent = parent->parent;
     }
     return parent;
   }
 
   leaf *min_rec(leaf *ptr) {
-    if (ptr->left->_sentinel) {
+    if (ptr->children[0]->_sentinel) {
       return ptr;
     }
-    return min_rec(ptr->left);
+    return min_rec(ptr->children[0]);
   }
 
   leaf *max_rec(leaf *ptr) {
-    if (ptr->right->_sentinel) {
+    if (ptr->children[1]->_sentinel) {
       return ptr;
     }
-    return max_rec(ptr->right);
+    return max_rec(ptr->children[1]);
   }
 
   leaf *find(int key) {
@@ -105,13 +114,13 @@ public:
     if (ptr->key == key)
       return ptr;
     if (ptr->key > key) {
-      if (ptr->left->_sentinel)
+      if (ptr->children[0]->_sentinel)
         return sentinel;
-      return find_rec(key, ptr->left);
+      return find_rec(key, ptr->children[0]);
     }
-    if (ptr->right->_sentinel)
+    if (ptr->children[1]->_sentinel)
       return sentinel;
-    return find_rec(key, ptr->right);
+    return find_rec(key, ptr->children[1]);
   }
 
   bool delete_node(int key) {
@@ -124,20 +133,20 @@ public:
       std::cerr << "NODE WAS NOT FOUND AND COULD NOT BE DELETED" << std::endl;
       return false;
     }
-    if (!tbdel.right->_sentinel && !tbdel.left->_sentinel) {
-      tbdel.value = min(tbdel.right)->value;
-      tbdel.key = min(tbdel.right)->key;
-      return delete_node_rec(*min(tbdel.right));
+    if (!tbdel.children[1]->_sentinel && !tbdel.children[0]->_sentinel) {
+      tbdel.value = min(tbdel.children[1])->value;
+      tbdel.key = min(tbdel.children[1])->key;
+      return delete_node_rec(*min(tbdel.children[1]));
     } else {
-      if (!tbdel.left->_sentinel) {
-        tbdel.key = tbdel.left->key;
-        tbdel.value = tbdel.left->value;
-        *tbdel.left = *sentinel;
+      if (!tbdel.children[0]->_sentinel) {
+        tbdel.key = tbdel.children[0]->key;
+        tbdel.value = tbdel.children[0]->value;
+        *tbdel.children[0] = *sentinel;
         return true;
-      } else if (!tbdel.right->_sentinel) {
-        tbdel.key = tbdel.right->key;
-        tbdel.value = tbdel.right->value;
-        *tbdel.right = *sentinel;
+      } else if (!tbdel.children[1]->_sentinel) {
+        tbdel.key = tbdel.children[1]->key;
+        tbdel.value = tbdel.children[1]->value;
+        *tbdel.children[1] = *sentinel;
       } else {
         tbdel = *sentinel;
       }
@@ -145,7 +154,106 @@ public:
     }
   }
 
-  bool insert(int key, std::string val) {
+#define LEFT 0
+#define RIGHT 1
+#define left children[LEFT]
+#define right children[RIGHT]
+
+  leaf *RotateDirRoot(leaf *P, int dir) { // dir ∈ { LEFT, RIGHT }
+    leaf *G = P->parent;
+    leaf *S = P->children[1 - dir];
+    leaf *C;
+    if (S->_sentinel)
+      return S; // pointer to true node required
+    C = S->children[dir];
+    P->children[1 - dir] = C;
+    if (!C->_sentinel)
+      C->parent = P;
+    S->children[dir] = P;
+    P->parent = S;
+    S->parent = G;
+    if (!G->_sentinel)
+      G->children[P == G->right ? RIGHT : LEFT] = S;
+    else
+      root = S;
+    return S; // new root of subtree
+  }
+
+#define RotateDir(N, dir) RotateDirRoot(N, dir)
+#define RotateLeft(N) RotateDirRoot(T, N, LEFT)
+#define RotateRight(N) RotateDirRoot(T, N, RIGHT)
+#define childDir(N) (N == (N->parent)->right ? RIGHT : LEFT)
+
+  bool RBinsert(int key, std::string val) {
+    int side = 0;
+    leaf *grandparent;
+    leaf *uncle;
+
+    if (!size) {
+      root = new leaf(key, val, sentinel, sentinel);
+      root->color = BLACK;
+      size++;
+      return true;
+    }
+    side = insert(key, val);
+    int dir = side - 1;
+
+    leaf *parent = cursor->parent;
+    leaf *P = cursor->parent;
+    cursor = find(key);
+
+    while (!(parent = cursor->parent)->_sentinel) {
+      // IF the parent is black its all good, just return
+      if (cursor->parent->color == BLACK)
+        return true;
+
+      // from now on we know the parent is red
+
+      if ((grandparent = parent->parent)
+              ->_sentinel) // if the grandparent is sentinel go to case 4
+        goto Case_I4;
+
+      // else parent is red and grandparent is a sentinel
+
+      dir = childDir(
+          parent); // on which side of the grandparent is parent located
+      uncle = grandparent->children[1 - dir]; // thats just how uncles work
+                                              //
+      if (uncle->_sentinel ||
+          uncle->color ==
+              BLACK) // if the uncle is a sentinel or is black go to case I56
+        goto Case_I56;
+      parent->color = BLACK;
+      uncle->color = BLACK;
+      grandparent->color = RED;
+      cursor = grandparent;
+    }
+
+  Case_I4: // P is the root and red:
+    parent->color = BLACK;
+    return true;                               // insertion complete
+                                               //
+  Case_I56:                                    // P red && U black:
+    if (cursor == parent->children[1 - dir]) { // Case_I5 (P red && U black && N
+                                               // inner grandchild of G):
+      RotateDir(parent, dir);                  // P is never the root
+      cursor = parent;                         // new current node
+      parent = grandparent->children[dir];     // new parent of N
+      // fall through to Case_I6
+    }
+    // end of RBinsert1
+    // Case_I6 (P red && U black && N outer grandchild of G):
+    RotateDirRoot(grandparent, 1 - dir); // G may be the root
+    parent->color = BLACK;
+    grandparent->color = RED;
+    return true; // insertion complete
+                 // end of RBinsert1
+  };
+
+  // insert can probably benefit from more synergy with search or find,
+  // especially since find returns the correctly positioned sentinel node if no
+  // matching node is found.
+  int insert(int key, std::string val) {
     if (!size) {
       root = new leaf(key, val, sentinel, sentinel);
       size++;
@@ -155,40 +263,46 @@ public:
     return insert_rec(key, val);
   }
 
-  bool insert_rec(int key, std::string val) {
+  int insert_rec(int key, std::string val) {
     if (val == cursor->value)
       return 0;
     if (key < cursor->key) {
-      if (cursor->left->_sentinel)
-        cursor->left = new leaf(key, val, cursor, sentinel);
+      if (cursor->children[0]->_sentinel)
+        cursor->children[0] = new leaf(key, val, cursor, sentinel);
       else {
-        cursor = cursor->left;
+        cursor = cursor->children[0];
         insert_rec(key, val);
       }
       return 1;
     } else {
-      if (cursor->right->_sentinel)
-        cursor->right = new leaf(key, val, cursor, sentinel);
+      if (cursor->children[1]->_sentinel)
+        cursor->children[1] = new leaf(key, val, cursor, sentinel);
       else {
-        cursor = cursor->right;
+        cursor = cursor->children[1];
         insert_rec(key, val);
       }
-      return 1;
+      return 2;
     }
     return false;
   };
 
   void printBT(const std::string &prefix, const leaf *node, bool isLeft) {
-    if (node != nullptr) {
+    if (!node->_sentinel) {
       std::cout << prefix;
       std::cout << (isLeft ? "├──" : "└──");
-      std::cout << node->value << std::endl;
-      printBT(prefix + (isLeft ? "│   " : "    "), node->left, true);
-      printBT(prefix + (isLeft ? "│   " : "    "), node->right, false);
+      //      std::system("Color E4");
+      //      std::cerr << "\x1B[31mTexting\033[0m\t\t" << std::endl;
+      if (node->color == RED)
+        std::cout << "\x1B[31m" << node->value << "\033[0m\t\t" << std::endl;
+      else
+        std::cout << node->value << std::endl;
+
+      printBT(prefix + (isLeft ? "│   " : "    "), node->children[0], true);
+      printBT(prefix + (isLeft ? "│   " : "    "), node->children[1], false);
     }
   }
 
-  void printBT() { printBT("", root, false); }
+  void printBT() { printBT(" ", root, false); }
 
 protected:
   leaf *sentinel;
@@ -197,24 +311,13 @@ protected:
   size_t size;
 };
 
-leaf *createNode(int key, std::string val) {
-  leaf *res = new leaf;
-  if (res) {
-    res->value = val;
-    res->key = key;
-    res->right = NULL;
-    res->left = NULL;
-  }
-  return res;
-}
-
 void printBT(const std::string &prefix, const leaf *node, bool isLeft) {
   if (node != nullptr) {
     std::cout << prefix;
     std::cout << (isLeft ? "├──" : "└──");
     std::cout << node->value << std::endl;
-    printBT(prefix + (isLeft ? "│   " : "    "), node->left, true);
-    printBT(prefix + (isLeft ? "│   " : "    "), node->right, false);
+    printBT(prefix + (isLeft ? "│   " : "    "), node->children[0], true);
+    printBT(prefix + (isLeft ? "│   " : "    "), node->children[1], false);
   }
 }
 
@@ -222,51 +325,83 @@ void printBT(const leaf *node) { printBT("", node, false); }
 
 int main() {
 
-  tree elm;
-  elm.insert(3, "three");
-  elm.insert(1, "one");
-  elm.insert(5, "five");
-  elm.insert(11, "eleven");
-  elm.insert(9, "nine");
-  elm.insert(2, "two");
-  elm.insert(4, "four");
-  elm.insert(10, "ten");
-  elm.insert(12, "twelve");
-  elm.insert(13, "thirteen");
-  elm.insert(5, "five");
-  elm.insert(6, "six");
-  elm.insert(7, "seven");
-  elm.insert(8, "eight");
-  elm.insert(8, "eight");
-  elm.insert(8, "eight");
-  elm.insert(8, "eight");
-  elm.insert(8, "eight");
-  elm.insert(8, "eight");
-  elm.printBT();
+  // tree elm;
+  // elm.insert(3, "three");
+  // elm.insert(1, "one");
+  // elm.insert(5, "five");
+  // elm.insert(11, "eleven");
+  // elm.insert(9, "nine");
+  // elm.insert(2, "two");
+  // elm.insert(4, "four");
+  // elm.insert(10, "ten");
+  // elm.insert(12, "twelve");
+  // elm.insert(13, "thirteen");
+  // elm.insert(5, "five");
+  // elm.insert(6, "six");
+  // elm.insert(7, "seven");
+  // elm.insert(8, "eight");
+  // elm.insert(8, "eight");
+  // elm.insert(8, "eight");
+  // elm.insert(8, "eight");
+  // elm.insert(8, "eight");
+  // elm.insert(8, "eight");
+  // elm.printBT();
 
-  std::cerr << "this is the next after 4 " << elm.next(4)->value << std::endl;
-  std::cerr << "this is the next after 1 " << elm.next(1)->value << std::endl;
-  std::cerr << "this is the next after 9 " << elm.next(9)->value << std::endl;
-  std::cerr << "this is the next after 3 " << elm.next(3)->value << std::endl;
-  std::cerr << "this is the prev before 4 " << elm.prev(4)->value << std::endl;
-  std::cerr << "this is the prev before 1 " << elm.prev(1)->value << std::endl;
-  std::cerr << "this is the prev before 9 " << elm.prev(9)->value << std::endl;
-  std::cerr << "this is the prev before 3 " << elm.prev(3)->value << std::endl;
+  // std::cerr << "this is the next after 4 " << elm.next(4)->value <<
+  // std::endl; std::cerr << "this is the next after 1 " << elm.next(1)->value
+  // << std::endl; std::cerr << "this is the next after 9 " <<
+  // elm.next(9)->value << std::endl; std::cerr << "this is the next after 3 "
+  // << elm.next(3)->value << std::endl; std::cerr << "this is the prev before 4
+  // " << elm.prev(4)->value << std::endl; std::cerr << "this is the prev before
+  // 1 " << elm.prev(1)->value << std::endl; std::cerr << "this is the prev
+  // before 9 " << elm.prev(9)->value << std::endl; std::cerr << "this is the
+  // prev before 3 " << elm.prev(3)->value << std::endl;
 
-  std::cerr << "\n\n";
-  std::cerr << elm.min()->value << std::endl;
-  std::cerr << elm.max()->value << std::endl;
-  std::cerr << elm.find(4)->value << std::endl;
-  std::cerr << elm.find(1)->value << std::endl;
-  std::cerr << elm.find(8)->value << std::endl;
-  std::cerr << elm.find(3)->value << std::endl;
-  std::cerr << elm.find(100)->value << std::endl;
-  elm.delete_node(3);
-  elm.delete_node(9);
-  elm.delete_node(7);
-  // elm.delete_node(2);
-  elm.printBT();
+  // std::cerr << "\n\n";
+  // std::cerr << elm.min()->value << std::endl;
+  // std::cerr << elm.max()->value << std::endl;
+  // std::cerr << elm.find(4)->value << std::endl;
+  // std::cerr << elm.find(1)->value << std::endl;
+  // std::cerr << elm.find(8)->value << std::endl;
+  // std::cerr << elm.find(3)->value << std::endl;
+  // std::cerr << elm.find(100)->value << std::endl;
+  // elm.delete_node(3);
+  // elm.delete_node(9);
+  // elm.delete_node(7);
+  //// elm.delete_node(2);
+  // elm.printBT();
 
+  tree oak;
+  oak.RBinsert(20, "twenty");
+  oak.RBinsert(3, "three");
+  oak.RBinsert(22, "twenty two");
+  oak.printBT();
+  oak.RBinsert(23, "twenty three");
+  oak.printBT();
+  oak.RBinsert(4, "four");
+  oak.printBT();
+  oak.RBinsert(5, "five");
+  oak.RBinsert(11, "eleven");
+  oak.RBinsert(9, "nine");
+  oak.printBT();
+  oak.printBT();
+  oak.RBinsert(2, "two");
+  oak.RBinsert(4, "four");
+  oak.RBinsert(10, "ten");
+  oak.RBinsert(12, "twelve");
+  oak.printBT();
+  oak.RBinsert(13, "thirteen");
+  oak.RBinsert(5, "five");
+  oak.RBinsert(6, "six");
+  oak.RBinsert(7, "seven");
+  oak.printBT();
+  oak.RBinsert(8, "eight");
+  oak.RBinsert(8, "eight");
+  oak.RBinsert(8, "eight");
+  oak.RBinsert(8, "eight");
+  oak.RBinsert(8, "eight");
+  oak.RBinsert(8, "eight");
+  oak.printBT();
   // tree oak;
   // oak.insert(1, elm.min()->value);
   // // elm.delete_node(elm.min()->key);
@@ -280,28 +415,6 @@ int main() {
   // // elm.delete_node(elm.min()->key);
   // // oak.insert(elm.min()->key, elm.min()->value);
   // // elm.delete_node(elm.min()->key);
-
-  tree oak;
-  oak.insert(3, "three");
-  oak.insert(1, "one");
-  oak.insert(5, "five");
-  oak.insert(11, "eleven");
-  oak.insert(9, "nine");
-  oak.insert(2, "two");
-  oak.insert(4, "four");
-  oak.insert(10, "ten");
-  oak.insert(12, "twelve");
-  oak.insert(13, "thirteen");
-  oak.insert(5, "five");
-  oak.insert(6, "six");
-  oak.insert(7, "seven");
-  oak.insert(8, "eight");
-  oak.insert(8, "eight");
-  oak.insert(8, "eight");
-  oak.insert(8, "eight");
-  oak.insert(8, "eight");
-  oak.insert(8, "eight");
-  // oak.printBT();
 
   return 0;
 }
